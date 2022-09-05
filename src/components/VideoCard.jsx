8e08b038bc2482035d58c14540b19800444bb74d
Mageshwaran_Kannivel <mageshwarankannivel@gmail.com>
2022-09-05 21:13:10 +0530

    firstcommit

---
+++ src/components/VideoCard.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Link } from "react-router-dom"
+
+import { Typography, Card, CardContent, CardMedia } from "@mui/material";
+import { CheckCircle } from "@mui/icons-material";
+
+import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelTitle, demoChannelUrl } from "../utils/constants";
+
+const VideoCard = ({ video: {id:{videoId}, snippet} }) => {
+  
+  return (
+    <Card>
+      <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+        <CardMedia image={snippet?.thumbnails?.high?.url}
+          alt={snippet?.title} sx={{ width: 358, height: 180 }}
+        />
+      </Link>
+
+      <CardContent sx={{ backroudColor: '#1e1e1e', height: '160px' }}>
+        <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+          <Typography>
+            {snippet?.title.slice(0,60)}
+          </Typography>
+        </Link>
+      </CardContent>
+      
+        
+      
+      
+   </Card>
+    
+   
+   
+  )
+}
+
+export default VideoCard
